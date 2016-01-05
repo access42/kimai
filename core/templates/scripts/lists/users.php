8a50f5e6eb5242e280cd0b1cc50d321a2767c2cc
Audrey <audreymaniez@gmail.com>
2016-01-05 23:15:14 +0100

    structure users footer list

--- core/templates/scripts/lists/users.php
+++ core/templates/scripts/lists/users.php
@@ -1,11 +1,17 @@
 <table>
+	<caption class="sr"><?php echo $this->kga['lang']['userslist'] ?></caption>
+	<tr class="sr">
+		<td scope="col"><?php echo $this->kga['lang']['actions'] ?></td>
+		<td scope="col"><?php echo $this->kga['lang']['users'] ?></td>
+		<td scope="col"><?php echo $this->kga['lang']['timeworking'] ?></td>
+	</tr>
   <tbody>
     <?php
     if (count($this->users) == 0)
     {
         ?>
         <tr>
-            <td nowrap colspan='3'>
+            <td colspan="3">
                 <?php echo $this->error(); ?>
             </td>
         </tr>
@@ -18,20 +24,20 @@
             ?>
             <tr id="row_user" data-id="<?php echo $user['userID']?>" class="<?php echo $this->cycle(array('odd','even'))->next()?>">
               <!--  option cell -->
-              <td nowrap class="option">
-                <a href="#" onclick="lists_update_filter('user',<?php echo $user['userID']?>); $(this).blur(); return false;"><img
-                        src='../skins/<?php echo $this->escape($this->kga['conf']['skin'])?>/grfx/filter.png' width='13' height='13'
-                        alt='<?php echo $this->kga['lang']['filter']?>' title='<?php echo $this->kga['lang']['filter']?>' border='0' />
-                </a>
+              <td class="option">
+                <button onclick="lists_update_filter('user',<?php echo $user['userID']?>); $(this).blur(); return false;"><img
+                        src="../skins/<?php echo $this->escape($this->kga['conf']['skin'])?>/grfx/filter.png" width="13" height="13"
+                        alt="<?php echo $this->kga['lang']['filter'].' '.$user['name']?>" title="<?php echo $this->kga['lang']['filter'].' '.$user['name']?>" border="0" />
+                </button>
               </td>
 
               <!-- name cell -->
-              <td width="100%" class="clients">
+              <td class="clients">
                 <?php echo $this->escape($user['name']) ?>
               </td>
 
               <!-- annotation cell -->
-              <td nowrap class="annotation"></td>
+              <td class="annotation"></td>
             </tr>
             <?php
         }
